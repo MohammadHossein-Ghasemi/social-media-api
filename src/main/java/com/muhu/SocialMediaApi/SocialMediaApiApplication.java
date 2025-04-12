10b484313f516cc4c51cd4da704d16f1cb7136b9
Mohammad Hossein <s.m.h.gh.1382@gmail.com>
2025-04-12 17:40:59 +0330

    Set up project.

---
+++ src/main/java/com/muhu/SocialMediaApi/SocialMediaApiApplication.java
@@ -0,0 +1,13 @@
+package com.muhu.SocialMediaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muhu/SocialMediaApi/SocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhu.SocialMediaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
